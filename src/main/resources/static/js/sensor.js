09165984edd5dc5dc8b147afce41f6068cd60bab
sejinsjn <sedad_@hotmail.de>
2021-05-31 20:16:06 +0200

    Auslagerung von JavaScript Code aus index.html zu sensor.js

--- src/main/resources/static/js/sensor.js
+++ src/main/resources/static/js/sensor.js
@@ -45,3 +45,68 @@ class Sensor{
         });
     }
 }
+
+class Map{
+    createMap(){
+        let marker = new Array();
+
+        let treeIcon = L.icon({
+            iconUrl: 'images/tree.png',
+
+            iconSize:     [60, 95], // size of the icon
+            iconAnchor:   [30, 94], // point of the icon which will correspond to marker's location
+            popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+        });
+
+        let mymap = L.map('mapid').setView([51.5145, 7.464772], 14);
+        L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+            attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+            maxZoom: 18,
+            id: 'mapbox/streets-v11',
+            tileSize: 512,
+            zoomOffset: -1,
+            accessToken: 'pk.eyJ1Ijoic2VqaW5zam4iLCJhIjoiY2twNnRrbnYwMTg1NjJwcjJ2cXc1Z2phciJ9.4VY4Kdvdx7Kdy5OAWOM4cg'
+        }).addTo(mymap);
+
+        let markerb000 = L.marker([51.510879, 7.464040], {icon: treeIcon}).addTo(mymap).bindPopup('Baum 000');
+        let markerb001 = L.marker([51.512875, 7.471388], {icon: treeIcon}).addTo(mymap).bindPopup('Baum 001');
+        let markerb002 = L.marker([51.514774, 7.454719], {icon: treeIcon}).addTo(mymap).bindPopup('Baum 002');
+
+        marker.push(markerb000);
+        marker.push(markerb001);
+        marker.push(markerb002);
+
+        $("#button").click( (e) => {
+                    $('html,body').animate({
+        scrollTop: $("#mapid").offset().top},
+        'fast');});
+
+       //disable default scroll
+      mymap.scrollWheelZoom.disable();
+
+      $("#mapid").bind('mousewheel DOMMouseScroll', (event) =>  {
+        event.stopPropagation();
+         if (event.ctrlKey == true) {
+                 event.preventDefault();
+             mymap.scrollWheelZoom.enable();
+             setTimeout(function(){
+                 mymap.scrollWheelZoom.disable();
+             }, 1000);
+         }
+         else{
+            //Add code to display "use ctrl to scroll" on map
+         }
+      });
+      return marker;
+    }
+
+    createChartsForMarker(sensor, title, marker){
+        marker.on('click',() => {
+            sensor.createChart(sensor, title, "Helligkeit", 0);
+            sensor.createChart(sensor, title, "Niederschlag", 1);
+            sensor.createChart(sensor, title, "Luftfeuchtigkeit", 2);
+            sensor.createChart(sensor, title, "Bodenfeuchtigkeit", 3);
+            sensor.createChart(sensor, title, "Temperatur", 4);
+        });
+    }
+}
